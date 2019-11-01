e7f6000ddb0c4c7002e31b1f8c17c179644c0d86
hoangvietonline <vietndh@maxmobile-software.com>
2019-11-01 11:28:26 +0700

    first commit

--- app/src/main/java/hoangviet/ndhv/demoui/MainActivity.java
+++ app/src/main/java/hoangviet/ndhv/demoui/MainActivity.java
@@ -2,12 +2,15 @@ package hoangviet.ndhv.demoui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.edtName);
+
     }
 }
